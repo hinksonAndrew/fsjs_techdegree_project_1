8e2c6a2fe6290405dec06361817ddc29ecb8daa4
Andrew Hinkson <anhinkson8842@hotmail.com>
2020-06-11 11:07:49 -0500

    Added some quotes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,58 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: "They may take our lives, but they'll never take our freedom!",
+    source: "Mel Gibson as William Wallace",
+    citation: "Braveheart",
+    year: "1995",
+    tag: "movie"
+  },
+  {
+    quote: "You complete me.",
+    source: "Tom Cruise as ",
+    citation: "Jerry Maguire",
+    year: "1996",
+    tag: "movie"
+  },
+  {
+    quote: "Chewie, we're home.",
+    source: "Harrison Ford as Han Solo ",
+    citation: "Star Wars Ep VII: The Force Awakens",
+    year: "2015",
+    tag: "movie"
+  },
+  {
+    quote: "They call it a Royale with cheese.",
+    source: "John Travolta as Vincent",
+    citation: "Pulp Fiction",
+    year: "1994",
+    tag: "movie"
+  },
+  {
+    quote: "I am the one who knocks.",
+    source: "Brian Cranston as Walter White",
+    citation: "Breaking Bad Season 4 Ep. 6",
+    year: "2011",
+    tag: "tv"
+  },
+  {
+    quote: "You complete me.",
+    source: "Tom Cruise as ",
+    citation: "Jerry Maguire",
+    year: "1996",
+    tag: "movie"
+  },
+  {
+    quote: "You complete me.",
+    source: "Tom Cruise as ",
+    citation: "Jerry Maguire",
+    year: "1996",
+    tag: "movie"
+  },
+];
+
 
 
 /***
@@ -30,4 +82,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
