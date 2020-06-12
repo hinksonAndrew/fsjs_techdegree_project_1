4183202673694cff9e6a5aab5e1fb7895f8533d9
Andrew Hinkson <anhinkson8842@hotmail.com>
2020-06-12 12:10:59 -0500

    Added functions to print HTML

--- js/script.js
+++ js/script.js
@@ -16,51 +16,49 @@ const quotes = [
     quote: "They may take our lives, but they'll never take our freedom!",
     source: "Mel Gibson as William Wallace",
     citation: "Braveheart",
-    year: "1995",
-    tag: "movie"
+    year: 1995,
+    tag: ["movie", "epic"]
   },
   {
     quote: "You complete me.",
-    source: "Tom Cruise as ",
+    source: "Tom Cruise as Jerry Maguire",
     citation: "Jerry Maguire",
-    year: "1996",
-    tag: "movie"
+    year: 1996,
+    tag: ["movie", "not watched"]
   },
   {
     quote: "Chewie, we're home.",
     source: "Harrison Ford as Han Solo ",
     citation: "Star Wars Ep VII: The Force Awakens",
-    year: "2015",
-    tag: "movie"
+    year: 2015,
+    tag: ["movie"]
   },
   {
     quote: "They call it a Royale with cheese.",
     source: "John Travolta as Vincent",
     citation: "Pulp Fiction",
-    year: "1994",
-    tag: "movie"
+    year: 1994,
+    tag: ["movie", "awesome"]
   },
   {
     quote: "I am the one who knocks.",
     source: "Brian Cranston as Walter White",
     citation: "Breaking Bad Season 4 Ep. 6",
-    year: "2011",
-    tag: "tv"
+    year: 2011,
+    tag: ["tv", "awesome"]
   },
   {
-    quote: "You complete me.",
-    source: "Tom Cruise as ",
-    citation: "Jerry Maguire",
-    year: "1996",
-    tag: "movie"
+    quote: "Rosebud.",
+    source: "Orson Welles as Kane",
+    citation: "Citizen Kane",
+    year: 1941,
+    tag: ["movie", "classic"]
   },
   {
-    quote: "You complete me.",
-    source: "Tom Cruise as ",
-    citation: "Jerry Maguire",
-    year: "1996",
-    tag: "movie"
-  },
+    quote: "I am going to stop procrastinating.",
+    source: "Andrew Hinkson",
+    citation: "Myself"
+  }
 ];
 
 
@@ -69,11 +67,41 @@ const quotes = [
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+  const randomNumber = Math.floor(Math.random()*quotes.length);
+  return quotes[randomNumber];
+}
 
+function getRandomRGB() {
+  return Math.floor(Math.random() * 256);
+}
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  document.getElementById('load-quote').removeEventListener("click", printQuote, false);
+  setInterval(function() {
+    let quoteObj = getRandomQuote();
+
+    document.body.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`
+
+    let html = `<p class="quote">${quoteObj.quote}</p>`;
+    html += `<p class="source">${quoteObj.source}`;
+    if (quoteObj.citation) {
+      html += `<span class="citation">${quoteObj.citation}</span>`
+    }
+    if (quoteObj.year) {
+      html += `<span class="year">${quoteObj.year}</span>`;
+    }
+    if (quoteObj.tag) {
+      html += `<span class="tag"> [${quoteObj.tag}]</span>`;
+    }
+    html += "</p>";
+    
+    return document.getElementById('quote-box').innerHTML = html;
+  }, 5000);
+}
 
 
 
@@ -82,4 +110,4 @@ const quotes = [
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
